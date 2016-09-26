2c32a8ba1ad0baed3d540213b2f21520d1ac3647
Aleksandr Didenko <adidenko@mirantis.com>
2016-09-26 10:05:28 +0200

    Remove deprecated kpm commands

--- deploy-k8s.kargo.sh
+++ deploy-k8s.kargo.sh
@@ -12,10 +12,6 @@ ansible-playbook -i $INVENTORY /root/kargo/cluster.yml -e @custom.yaml
 deploy_res=$?
 
 if [ "$deploy_res" -eq "0" ]; then
-  echo "Setting up kubedns..."
-  ansible-playbook -i $INVENTORY playbooks/kubedns.yaml
-  echo "Setting up kubedashboard..."
-  ansible-playbook -i $INVENTORY playbooks/kubedashboard.yaml
   echo "Setting up ip route work-around for DNS clusterIP availability..."
   ansible-playbook -i $INVENTORY playbooks/ipro_for_cluster_ips.yaml
 fi
