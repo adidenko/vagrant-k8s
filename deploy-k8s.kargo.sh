6fd02cf109a2d9848075bee0eb1e75d223095c95
Aleksandr Didenko <adidenko@mirantis.com>
2016-10-04 12:08:29 +0200

    Update work-around playbooks

--- deploy-k8s.kargo.sh
+++ deploy-k8s.kargo.sh
@@ -14,6 +14,6 @@ ansible-playbook -i $INVENTORY /root/kargo/cluster.yml -e @${CUSTOM}
 deploy_res=$?
 
 if [ "$deploy_res" -eq "0" ]; then
-  echo "Setting up ip route work-around for DNS clusterIP availability..."
-  ansible-playbook -i $INVENTORY playbooks/ipro_for_cluster_ips.yaml
+  echo "Setting up resolv.conf ..."
+  ansible-playbook -i $INVENTORY playbooks/resolv_conf.yaml
 fi
