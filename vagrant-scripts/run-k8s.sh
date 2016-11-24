4e382b5d38bbebbc77bcb3cb7bb7ef24427f3172
Aleksandr Didenko <adidenko@mirantis.com>
2016-11-24 12:22:24 +0100

    Add new script to deploy k8s and test network

---
+++ vagrant-scripts/run-k8s.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+
+#export KARGO_COMMIT="fix-ansible_ssh_user"
+#export KARGO_REPO="https://github.com/adidenko/kargo"
+
+git clone https://github.com/adidenko/vagrant-k8s ~/mcp
+
+pushd ~/mcp
+# Step 1
+bash -x ./bootstrap-master.sh
+
+export INVENTORY=`pwd`/nodes_to_inv.py
+export K8S_NODES_FILE=`pwd`/nodes
+
+# Test connectivity
+cat nodes
+ansible all -m ping -i $INVENTORY
+
+# Deploy cluster
+bash -x deploy-k8s.kargo.sh
+
+# Test network
+ansible-playbook -i $INVENTORY playbooks/tests.yaml -e @ccp.yaml
+
+popd
