9d4f74ce1336aa9dd0a7e2266102c78d295e6046
Aleksandr Didenko <adidenko@mirantis.com>
2016-12-09 10:36:38 +0100

    Add cluster_id for calico-rr hostvars

--- nodes_to_inv.py
+++ nodes_to_inv.py
@@ -55,7 +55,9 @@ def nodes_to_hash(nodes_list, masters, calico_rrs, group_vars):
         }
 
         if i <= calico_rrs:
+            cluster_id = "1.0.0.%s" % i
             nodes['calico-rr']['hosts'].append(node_name)
+            nodes['_meta']['hostvars'][node_name]['cluster_id'] = cluster_id
             continue
 
         nodes['kube-node']['hosts'].append(node_name)
